8292e8f30718796dc0a689f3fc356bcc463d7f7b
Fernando <nando.neves@gmail.com>
2020-09-15 18:34:35 -0300

    classes criadas

---
+++ script.js
@@ -0,0 +1,7 @@
+// Grupos das classes
+
+// `newspaper`, `magazine1`, `magazine2` (Grupo estilo)
+// - `medium`, `big`, `reallybig` (Grupo tamanho)
+// - `rotateleft`, `rotateright` (Grupo rotação)
+// - `skewleft`, `skewright` (Grupo inclinação)
+
