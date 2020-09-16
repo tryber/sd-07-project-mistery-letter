0b86c700bbc85871e0f6420235591e8881129d54
Danwhat <daanwhat@gmail.com>
2020-09-16 15:39:13 -0300

    first commit almost 100% I hope so

---
+++ script.js
@@ -0,0 +1,30 @@
+const carta = document.querySelector('#carta-gerada')
+
+const grupoEstilo = ['newspaper', 'magazine1', 'magazine2']
+const grupoTamanho = ['medium', 'big', 'reallybig']
+const grupoRotacao = ['rotateleft', 'rotateright',]
+const grupoInclinacao = ['skewright', 'skewleft']
+
+function setRandomStyles(word) {
+  function setRandom(array) {return Math.floor(Math.random()* array.length)}
+  word.classList.add(grupoEstilo[setRandom(grupoEstilo)])
+  word.classList.add(grupoTamanho[setRandom(grupoTamanho)])
+  word.classList.add(grupoRotacao[setRandom(grupoRotacao)])
+  word.classList.add(grupoInclinacao[setRandom(grupoInclinacao)])
+}
+
+function criarFraseFeia() {
+  const fraseUsuario = document.querySelector('#carta-texto').value
+  const arrayWords = fraseUsuario.split(" ")
+  carta.innerHTML = ''
+  for (let i = 0; i < arrayWords.length; i += 1) {
+    const word = document.createElement('span')
+    word.innerHTML = arrayWords[i]
+    setRandomStyles(word)
+    carta.appendChild(word)
+    carta.innerHTML += ' '
+  }
+}
+
+const btn = document.querySelector('#criar-carta')
+btn.addEventListener('click', criarFraseFeia)
