0a7da9551e642a425a8b65ac0f31eda69dbb10d0
Luiz F S Mariz <lfsmariz@gmail.com>
2020-09-16 19:07:51 -0300

    create const

---
+++ script.js
@@ -0,0 +1,13 @@
+const classTypes = {
+  styleGroup: ['newspaper', 'magazine1', 'magazine2'],
+  sizeGroup : ['medium', 'big', 'reallybig'],
+  rotateGroup: ['rotateleft', 'rotateright'],
+  skewGroup: ['skewleft', 'skewright'],
+}
+
+const buttonGenerateLetter = document.querySelector('#criar-carta')
+const inputLetter = document.querySelector('#carta-texto')
+const stylizedLetter = document.querySelector('#carta-gerada')
+const countWords = document.querySelector('#carta-contador')
+
+
