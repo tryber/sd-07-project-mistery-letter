bcbc09ae778ce84cfff8a2db1a48778724e2d139
Berilo Wanderley <wberilo@gmail.com>
2020-09-16 16:05:48 -0300

    cc

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ const style = ['newspaper', 'magazine1', 'magazine2'];
 const size = ['medium', 'big', 'reallybig'];
 const rot = ['rotateleft', 'rotateright'];
 const inc = ['skewleft', 'skewright'];
-document.querySelector("#criar-carta").addEventListener("click", function () {
-  if(document.querySelector("#carta-texto").value === ''){
-    document.querySelector('#carta-contador').innerText = 'por favor, digite o conteúdo da carta.'
+document.querySelector('#criar-carta').addEventListener('click', function () {
+  if (document.querySelector('#carta-texto').value === '') {
+    document.querySelector('#carta-gerada').innerText = 'Por favor, digite o conteúdo da carta.';
     return;
   }
-  const str = document.querySelector("#carta-texto").value.split(" ");
+  const str = document.querySelector('#carta-texto').value.split(' ');
   const div = document.createElement('div');
   for (let index = 0; index < str.length; index += 1) {
     const word = document.createElement('span');
-    word.innerText = str[index]
-    word.classList.add(style[Math.floor(Math.random() * 3)]) 
-    word.classList.add(size[Math.floor(Math.random() * 3)]) 
-    word.classList.add(rot[Math.floor(Math.random() * 2)]) 
-    word.classList.add(inc[Math.floor(Math.random() * 2)])
-    div.appendChild(word)
+    word.innerText = str[index];
+    word.classList.add(style[Math.floor(Math.random() * 3)]);
+    word.classList.add(size[Math.floor(Math.random() * 3)]);
+    word.classList.add(rot[Math.floor(Math.random() * 2)]);
+    word.classList.add(inc[Math.floor(Math.random() * 2)]);
+    div.appendChild(word);
   }
-  document.querySelector("#carta-gerada").innerHTML = div.innerHTML;
+  document.querySelector('#carta-gerada').innerHTML = div.innerHTML;
   document.querySelector('#carta-contador').innerText = str.length;
 });
