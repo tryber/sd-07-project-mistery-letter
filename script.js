811c05fe423b081214c0368b366ee078f61099cd
Berilo Wanderley <wberilo@gmail.com>
2020-09-16 15:39:54 -0300

    generate

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-const style = [`newspaper`, `magazine1`, `magazine2`]
-const size = [`medium`, `big`, `reallybig` ]
-const rot = [`rotateleft`, `rotateright`]
-const inc = [`skewleft`, `skewright`]
+const style = ['newspaper', 'magazine1', 'magazine2'];
+const size = ['medium', 'big', 'reallybig'];
+const rot = ['rotateleft', 'rotateright'];
+const inc = ['skewleft', 'skewright'];
+document.querySelector("#criar-carta").addEventListener("click", function () {
+  const str = document.querySelector("#carta-texto").value.split(" ");
+  for (let index = 0; index < str.length; index += 1) {
+    const word = document.createElement('span');
+    word.innerText = str[index]
+    word.classList.add(style[Math.floor(Math.random() * 3)]) 
+    word.classList.add(size[Math.floor(Math.random() * 3)]) 
+    word.classList.add(rot[Math.floor(Math.random() * 2)]) 
+    word.classList.add(inc[Math.floor(Math.random() * 2)])
+    document.querySelector("#carta-gerada").appendChild(word)
+  }
+
+});
