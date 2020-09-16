e581684e6b4cf1d53705913eb6988b29cbc6f789
Vladimir <vladimirrchagas@gmail.com>
2020-09-16 10:14:53 -0300

    verificando requisitos

--- script.js
+++ script.js
@@ -1,55 +1,61 @@
+// Define constants
+const button = document.querySelector('#criar-carta');
+const classRotation = ['rotateleft', 'rotateright'];
+const classInclination = ['skewleft', 'skewright'];
+const classSize = ['medium', 'big', 'reallybig'];
+const classStyle = ['newspaper', 'magazine1', 'magazine2'];
+const paragraph = document.querySelector('#carta-gerada');
+const inputText = document.querySelector('#carta-texto');
 
-window.onload = function (){
-  // Define constants
-  const button = document.querySelector('#criar-carta');
-  const classRotation = ['rotateleft', 'rotateright'];
-  const classInclination = ['skewleft', 'skewright'];
-  const classSize = ['medium', 'big', 'reallybig'];
-  const classStyle = ['newspaper', 'magazine1', 'magazine2'];
-  const paragraph = document.querySelector('#carta-gerada');
-  const inputText = document.querySelector('#carta-texto');
-  const wordCountShow = document.querySelector('#carta-contador');
-  let wordCount = 0;
+function styleText(event) {
+  // Random constat to get numbers on arrays class
+  const rotationClassValue = classRotation[Math.round(Math.random() * 1)];
+  const inclinationClassValue = classInclination[Math.round(Math.random() * 1)];
+  const classSizeValue = classSize[Math.round(Math.random() * 2)];
+  const classStyleValue = classStyle[Math.round(Math.random() * 2)];
 
-  function styleText(event) {
-    // Random constat to get numbers on arrays class
-    const rotationClassValue = classRotation[Math.round(Math.random() * 1)];
-    const inclinationClassValue = classInclination[Math.round(Math.random() * 1)];
-    const classSizeValue = classSize[Math.round(Math.random() * 2)];
-    const classStyleValue = classStyle[Math.round(Math.random() * 2)];
-
-    event.className = `${rotationClassValue} ${inclinationClassValue} ${classSizeValue} ${classStyleValue}`;
-  }
-
-  function eventListenerWords() {
-    const words = document.querySelectorAll('#words');
-
-    for (let index = 0; index < words.length; index += 1) {
-      words[index].addEventListener('click', function () {
-        styleText(words[index]);
-      });
-    }
-  }
-
-  function createLetter() {
-    if (inputText.value !== '') {
-      const letterPrhase = inputText.value.split(' ');
-      wordCount += letterPrhase.length;
-      wordCountShow.innerText = wordCount;
-
-      for (let index = 0; index < letterPrhase.length; index += 1) {
-        const span = document.createElement('span');
-        span.id = 'words';
-        paragraph.appendChild(span);
-        paragraph.lastElementChild.innerText = letterPrhase[index];
-        styleText(paragraph.lastElementChild);
-      }
-      eventListenerWords();
-    } else {
-      paragraph.innerText = 'Por favor, digite o conteúdo da carta.';
-    }
-  }
-
-  button.addEventListener('click', createLetter);
+  event.className = `${rotationClassValue} ${inclinationClassValue} ${classSizeValue} ${classStyleValue}`;
 }
 
+function eventListenerWords() {
+  const words = document.querySelectorAll('#words');
+
+  for (let index = 0; index < words.length; index += 1) {
+    words[index].addEventListener('click', function () {
+      styleText(words[index]);
+    });
+  }
+}
+
+function createLetter() {
+  if (inputText.value.trim() !== '') {
+    const letterPrhase = inputText.value.split(' ');
+    const wordCountShow = document.querySelector('#carta-contador');
+    const wordCount = letterPrhase.length;
+    wordCountShow.innerText = wordCount;
+
+    for (let index = 0; index < letterPrhase.length; index += 1) {
+      const span = document.createElement('span');
+      span.id = 'words';
+      paragraph.appendChild(span);
+      paragraph.lastElementChild.innerText = letterPrhase[index];
+      styleText(paragraph.lastElementChild);
+    }
+    eventListenerWords();
+  } else {
+    paragraph.innerText = 'Por favor, digite o conteúdo da carta.';
+  }
+}
+
+function clearLetter() {
+  const words = document.querySelectorAll('#words');
+
+  for (let index = 0; index < words.length; index += 1) {
+    paragraph.removeChild(words[index]);
+  }
+}
+
+button.addEventListener('click', function () {
+  clearLetter();
+  createLetter();
+});
