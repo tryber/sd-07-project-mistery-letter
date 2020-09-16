07205cd12bedf466651695d0b530a6503a35db3c
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2020-09-16 19:51:25 -0300

    Testando um requisito

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ let texto = document.querySelector("#carta-gerada");
 let input = document.querySelector("#carta-texto");
 
 function adicionaSpam() {
+  let re = new RegExp("[0-9A-z] +[A-z0-9 ]+");
+  if (input.value == "" || input.value.search(re) != 0) {
+    texto.innerText = "Por favor, digite o conteúdo da carta.";
+  }
+
   for (let i = 0; i < input.value.split(" ").length; i += 1) {
     let span = document.createElement("span");
     texto.appendChild(span);
@@ -11,3 +16,5 @@ function adicionaSpam() {
 }
 
 button.addEventListener("click", adicionaSpam);
+
+//-------------------------------------------------------
