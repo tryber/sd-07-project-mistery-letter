a6c4e64b401a99c69153bb3975b8a10dc8649459
Fernando <nando.neves@gmail.com>
2020-09-15 23:38:43 -0300

    teste pra ver se passa

--- script.js
+++ script.js
@@ -12,26 +12,14 @@ criarFrase.addEventListener("click",function(){
     }
     if (naoVazio==1){
 
-        document.querySelector("#carta-gerada").innerHTML=""
-        // frase.value= frase.value + " "          // AINDA NÃO CONSEGUI RESOLVER ESSA GAMBIARRA
+        // document.querySelector("#carta-gerada").innerHTML=""
+        
         let contador = 0
         for (let index=0;index<=frase.value.length;index+=1){
 
-        // if(frase.value[index]!=" "){
-        //     palavra = palavra +frase.value[index];
-        // } else if (index == tamanho || frase.value[index]== " ") {
-        //     let nova = document.createElement('span');
-        //     nova.innerHTML = palavra;
-        //     console.log(index)
-        //     incluirEstilos(nova);
-        //     document.querySelector("#carta-gerada").appendChild(nova)
-        //     contador +=1;
-        //     palavra="";
-        // }
         if(index == tamanho || frase.value[index]== " "){
             let nova = document.createElement('span');
             nova.innerHTML = palavra;
-            console.log(index)
             incluirEstilos(nova);
             document.querySelector("#carta-gerada").appendChild(nova)
             contador +=1;
@@ -40,12 +28,11 @@ criarFrase.addEventListener("click",function(){
             palavra = palavra +frase.value[index];
         }
     } 
-    // document.querySelector("#carta-texto").value[document.querySelector("#carta-texto").value.length-1] = ""; 
+    let novaLinha = document.createElement('br');
+    document.querySelector("#carta-gerada").appendChild(novaLinha);
     document.querySelector("#carta-contador").innerHTML = contador; //Incluindo contador
-    //criarEventos;
     } else{
         document.querySelector("#carta-gerada").innerHTML=""
-        console.log("passou aqui")
         let nova = document.createElement('span');
         nova.innerHTML= "Por favor, digite o conteúdo da carta."
         document.querySelector("#carta-gerada").appendChild(nova)
@@ -63,7 +50,6 @@ let grupoInclinacao = ["skewleft", "skewright"];
 // INCLUIR ESTILOS
 
 function incluirEstilos(nova){
-    console.log("incluindo estilos");
     let x= Math.floor(Math.random() * grupoEstilo.length);
         let y= Math.floor(Math.random() * grupoTamanho.length);
         let z= Math.floor(Math.random() * grupoNotacao.length);
@@ -75,12 +61,10 @@ function incluirEstilos(nova){
 
 function criarEventos(){
     let todaAFrase = document.querySelectorAll(".item");
-    console.log("entrou no criar eventos")
     for (let i = 0; i < todaAFrase.length; i+=1) {
         (function(i) {
         todaAFrase[i].addEventListener("click", function() {
             incluirEstilos(todaAFrase[i]);
-            console.log("passou nessa parte")
          })
         })(i);
   }
