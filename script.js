7b9483a8343cdc5b320e628136ce162dac37fd52
danielmlduarte <danielduarte.tryber@gmail.com>
2020-09-15 22:11:50 -0300

    Criando regras de estilo para newspaper e magazine1

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 const buttonCreateLetter = document.querySelector('#criar-carta');
 
 function createSpan(word) {
-
   const spanElement = document.createElement('span');
-
-  spanElement.innerText = ` ${word} `;
+  spanElement.innerText = ` ${word}`;
   return spanElement;
 }
 
 buttonCreateLetter.addEventListener('click', function () {
   const cartaGerada = document.querySelector('#carta-gerada');
   const letterText = document.querySelector('#carta-texto').value;
-  if  (letterText.trim() === '') {
+  if (letterText.trim() === '') {
     document.querySelector('#carta-gerada').innerText = ('Por favor, digite o conteúdo da carta.');
   } else {
     cartaGerada.innerHTML = '';
     const arrayLetterText = letterText.split(' ');
     for (let index = 0; index < arrayLetterText.length; index += 1) {
-      if (arrayLetterText[index] != '') {
+      if (arrayLetterText[index] !== '') {
         cartaGerada.appendChild(createSpan(arrayLetterText[index]));
       }
     }
