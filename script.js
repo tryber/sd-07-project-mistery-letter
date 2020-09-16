6488eb682d844e84482c02c0a958937cc917e053
Julio Kauer <juliokauer@gmail.com>
2020-09-16 18:38:18 -0300

    Corrigindo tab

--- script.js
+++ script.js
@@ -1,39 +1,18 @@
+function randomItem(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
 function randomClasses() {
-  let classes = [];
-  let randomNumber = Math.random();
-  if (randomNumber < 0.33) {
-    classes.push('newspaper');
-  } else if (randomNumber < 0.66) {
-    classes.push('magazine1');
-  } else {
-    classes.push('magazine2');
-  }
-  randomNumber = Math.random();
-  if (randomNumber < 0.33) {
-    classes.push('medium');
-  } else if (randomNumber < 0.66) {
-    classes.push('big');
-  } else {
-    classes.push('reallybig');
-  }
-  randomNumber = Math.random();
-  if (randomNumber < 0.33) {
-    classes.push('rotateleft');
-  } else if (randomNumber < 0.66) {
-    classes.push('rotateright');
-  }
-  randomNumber = Math.random();
-  if (randomNumber < 0.33) {
-    classes.push('skewleft');
-  } else if (randomNumber < 0.66) {
-    classes.push('skewright');
-  }
+  const classes = [];
+  classes.push(randomItem(['newspaper','magazine1','magazine2']));
+  classes.push(randomItem(['medium','big','reallybig']));
+  classes.push(randomItem(['rotateleft','rotateright','']));
+  classes.push(randomItem(['skewleft','skewright','']));
   return classes.join(' ');
 }
 function criarCarta() {
   const cartaTexto = document.querySelector('#carta-texto');
   const cartaGerada = document.querySelector('#carta-gerada');
-  const words = cartaTexto.value.split(' ');
+  const words = cartaTexto.value.replace(/\t/g, '').split(' ');
 
   cartaGerada.innerHTML = '';
 
