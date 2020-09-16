82aa81bcc29f5bce3cf0b2f0fb281ec5bbf1c4dd
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-09-16 14:02:16 -0300

    se o texto for vazio, a mensagem e impressa no body da carta

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const createLetterButton = document.getElementById('criar-carta');
 
 function generateLetterWord() {
-  const letterWords = document.getElementById('carta-texto').value.split(' ');
-  if (letterWords.length > 0) {
-    const letterBody = document.getElementById('carta-gerada');
+  const letterWords = document.getElementById('carta-texto').value.trim().split(' ');
+  const letterBody = document.getElementById('carta-gerada');
+  if (letterWords.length > 1 && letterWords[0] !== '') {
     for (let i = 0; i < letterWords.length; i += 1) {
       const word = document.createElement('span');
       word.innerText = letterWords[i];
       letterBody.appendChild(word);
     }
   } else {
-      alert('Por favor, digite o conteúdo da carta.');
+      letterBody.innerText = 'Por favor, digite o conteúdo da carta.';
   }
 }
 
