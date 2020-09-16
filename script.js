93f2aa122af461342627df6f8d870ffeb9ac7fba
kramersilva <kramerscs@gmail.com>
2020-09-16 11:39:26 -0300

    requisitos 4 e 5

--- script.js
+++ script.js
@@ -2,13 +2,23 @@ const inputText = document.getElementById('carta-texto');
 const createLetterButton = document.getElementById('criar-carta');
 const letterParagraph = document.getElementById('carta-gerada');
 
+letterParagraph.childNodes.length
+
+function clearParagraph () {
+  let paragraphChildren = letterParagraph.childNodes
+  for (let index = 0; index < paragraphChildren.length; index += 1) {
+    letterParagraph.removeChild(paragraphChildren[index])
+  }
+}
+
 function createLetter() {
-  if(inputText.value) {
+  if(inputText.value && inputText.value.trim() !== "") {
+    clearParagraph()
     let newParagraphElement = document.createElement('span');
     newParagraphElement.innerText = inputText.value
     letterParagraph.appendChild(newParagraphElement)
   } else {
-    alert('Por favor, digite o conteúdo da carta.');
+    letterParagraph.innerText = 'Por favor, digite o conteúdo da carta.';
   }
 }
 
