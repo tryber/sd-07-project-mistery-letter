9863c9096fdf09b5f7f6e35ec640683139cb7cec
danielmlduarte <danielduarte.tryber@gmail.com>
2020-09-15 22:03:08 -0300

    Adicionando tratamento de input, excluindo espaços entre as palavras

---
+++ script.js
@@ -0,0 +1,25 @@
+const buttonCreateLetter = document.querySelector('#criar-carta');
+
+function createSpan(word) {
+
+  const spanElement = document.createElement('span');
+
+  spanElement.innerText = ` ${word} `;
+  return spanElement;
+}
+
+buttonCreateLetter.addEventListener('click', function () {
+  const cartaGerada = document.querySelector('#carta-gerada');
+  const letterText = document.querySelector('#carta-texto').value;
+  if  (letterText.trim() === '') {
+    document.querySelector('#carta-gerada').innerText = ('Por favor, digite o conteúdo da carta.');
+  } else {
+    cartaGerada.innerHTML = '';
+    const arrayLetterText = letterText.split(' ');
+    for (let index = 0; index < arrayLetterText.length; index += 1) {
+      if (arrayLetterText[index] != '') {
+        cartaGerada.appendChild(createSpan(arrayLetterText[index]));
+      }
+    }
+  }
+});
