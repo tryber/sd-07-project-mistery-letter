fc1c86bd8295d80fb62d4682d7a2d9c579780fa9
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-09-16 15:53:34 -0300

    adicionado contador de palavras da carta

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ const styleGroup = ['newspaper', 'magazine1', 'magazine2'];
 const sizeGroup = ['medium', 'big', 'reallybig'];
 const rotateGroup = ['rotateleft', 'rotateright'];
 const tiltgroup = ['skewleft', 'skewright'];
+const couterText = document.getElementById('carta-contador');
+let counter = 0;
 
 function generateLetterWord() {
   const letterWords = document.getElementById('carta-texto').value.trim().split(' ');
   const letterBody = document.getElementById('carta-gerada');
   letterBody.innerText = '';
   if (letterWords.length > 1 && letterWords[0] !== '') {
+    counter = letterWords.length;
     for (let i = 0; i < letterWords.length; i += 1) {
       const word = document.createElement('span');
       word.innerText = letterWords[i];
@@ -17,6 +20,7 @@ function generateLetterWord() {
   } else {
       letterBody.innerText = 'Por favor, digite o conteúdo da carta.';
   }
+  couterText.innerText = `Número de palavras: ${counter}`;
 }
 
 function setRandomClasses() {
