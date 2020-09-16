36157e748e75b1b8c4409652b4759db70292ad03
rafaelmguimaraes <rafaelmguimaraes@gmail.com>
2020-09-16 16:55:21 -0300

    Requirement 16

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 function generateLetter() {
+  
   const wordsOfLetter = (document.getElementById('carta-texto').value).toString().trim().split(' ');
   if (wordsOfLetter[0] !== '') {
     document.getElementById('carta-gerada').innerHTML = '';
     for (let indexWord in wordsOfLetter) {
       const wordSpan = document.createElement('span');
       wordSpan.innerText = wordsOfLetter[indexWord];
+      wordSpan.style.display = 'inline-block';
+      applyRandomClass(wordSpan);
       document.getElementById('carta-gerada').appendChild(wordSpan);
     }
   } else {
@@ -12,6 +15,26 @@ function generateLetter() {
   }
 }
 
+function applyRandomClass(element) { 
+  const classGroupStyle = ['newspaper', 'magazine1', 'magazine2'];
+  const classGroupLength = ['medium', 'big', 'reallybig'];
+  const classGroupRotation = ['rotateleft', 'rotateright'];
+  const classGroupSkew = ['skewleft', 'skewright'];
+  const allClassGroup = [classGroupStyle, classGroupLength, classGroupRotation, classGroupSkew];
+
+  const quantityOfClass = Math.ceil(Math.random() * allClassGroup.length);
+  for (let index = 0; index < quantityOfClass; index += 1) {
+    let randomClassGroupIndex, randomClassIndex;
+    do {
+      randomClassGroupIndex = Math.floor(Math.random() * allClassGroup.length);
+      randomClassIndex = Math.floor(Math.random() * allClassGroup[randomClassGroupIndex].length);
+    }
+    while (element.classList.contains(allClassGroup[randomClassGroupIndex][randomClassIndex]));
+    element.classList.add(allClassGroup[randomClassGroupIndex][randomClassIndex]);
+  }
+}
+
 window.onload = function() {
   document.getElementById('criar-carta').addEventListener('click',generateLetter);
+
 }
