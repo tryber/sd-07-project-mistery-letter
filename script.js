dc0295e224f7fa7d33ec58fdc0fe19da5562cddd
Pablo Pessanha <pablopessanh@gmail.com>
2020-09-19 18:13:28 -0300

    All requests done; refactoring #8

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const estilo = ['newspaper', 'magazine1', 'magazine2'];
 const rotacao = ['rotateleft', 'rotateright'];
 const inclinacao = ['skewleft', 'skewright'];
 
-// Solução p/ espaçamento em branco encontrado no seguinte link: https://bit.ly/3iGxe4F
 function checarEspaco(texto) {
   const checar = texto.replace(/\s/g, '');
   return checar;
@@ -23,22 +22,15 @@ function escolhaRandomica() {
 }
 
 novaCarta.addEventListener('click', () => {
-  let caixaTexto = textoInserido.value;
+  let caixaTexto = textoInserido.value.replace(/\s\s+/g, ' ').trim();
+  containerCarta.innerText = '';
 
   if (checarEspaco(caixaTexto).length !== 0) {
-    caixaTexto = caixaTexto.replace(/\s\s+/g, ' ').trim();
-    console.log(caixaTexto);
     const arrayTexto = caixaTexto.split(' ', caixaTexto.length - 1);
-
-    if (containerCarta.classList.contains('aviso')) {
-      containerCarta.classList.remove('aviso');
-      containerCarta.innerText = '';
-    }
 
     arrayTexto.forEach((textoSeparado) => {
       const classes = escolhaRandomica();
       const carta = document.createElement('span');
-
       carta.id = 'criar-carta';
 
       classes.forEach((item) => {
@@ -48,16 +40,12 @@ novaCarta.addEventListener('click', () => {
       const texto = document.createTextNode(textoSeparado);
       carta.appendChild(texto);
       containerCarta.appendChild(carta);
+
       contador.innerText = '';
       contador.innerText = arrayTexto.length;
     });
   } else {
     contador.innerText = '0';
-    containerCarta.innerText = '';
-    const aviso = 'Por favor, digite o conteúdo da carta.';
-    const texto = document.createTextNode(aviso);
-
-    containerCarta.classList.add('aviso');
-    containerCarta.appendChild(texto);
+    containerCarta.innerText = 'Por favor, digite o conteúdo da carta.';
   }
 });
