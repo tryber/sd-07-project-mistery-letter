78a2b6074b56246f57c33953c7a4437a5b93addc
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2020-09-19 11:42:48 -0300

    Testando

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
 let textoDaCarta;
 let arrayCarta = [];
 // https://stackoverflow.com/a/50135988 fonte .replace()
-document.getElementById('carta-texto').addEventListener('keydown', (e) => {
+document.getElementById('carta-texto').addEventListener('keydown', () => {
   textoDaCarta = document.getElementById('carta-texto').value.replace(/(^|<\/?[^>]+>|\s+)([^\s<]+)/g, '$1<span>$2</span>');
 });
 
-function numeroAleatorio() {
-  const numero = Math.floor(Math.random() * 3);
+function numeroAleatorio(n) {
+  const numero = Math.floor(Math.random() * n);
   return numero;
 }
 function aplicarEstilo() {
   const grupoEstilo = ['newspaper', 'magazine1', 'magazine2'];
-  const grupoRotacao = ['rotateleft', 'rotateleft', 'rotateright'];
-  const grupoInclinacao = ['skewleft', 'skewleft', 'skewright'];
+  const grupoRotacao = ['rotateleft', 'rotateright'];
+  const grupoInclinacao = ['skewleft', 'skewright'];
   const grupoTamanho = ['medium', 'big', 'reallybig'];
   document.querySelectorAll('span').forEach((item) => {
-    item.classList.add(grupoEstilo[numeroAleatorio()]);
-    item.classList.add(grupoTamanho[numeroAleatorio()]);
-    item.classList.add(grupoRotacao[numeroAleatorio()]);
-    item.classList.add(grupoInclinacao[numeroAleatorio()]);
+    item.className = '';
+    item.classList.add(grupoEstilo[numeroAleatorio(3)]);
+    item.classList.add(grupoTamanho[numeroAleatorio(3)]);
+    item.classList.add(grupoRotacao[numeroAleatorio(2)]);
+    item.classList.add(grupoInclinacao[numeroAleatorio(2)]);
   });
+}
+
+function aplicarEstiloNoClick(){
+  document.querySelectorAll('span').forEach((item) => {
+    item.addEventListener('click', () => {
+      aplicarEstilo();
+    })
+  })
 }
 
 function gerarCartaMisteriosa() {
   const linha = document.createElement('br');
   const carta = document.createElement('span');
-  carta.id = 'carta-gerada';
   carta.innerHTML = textoDaCarta;
   document.getElementById('carta-gerada').appendChild(carta);
   document.getElementById('carta-gerada').appendChild(linha);
@@ -33,16 +41,16 @@ function gerarCartaMisteriosa() {
 
 document.getElementById('criar-carta').addEventListener('click', () => {
   if (document.querySelector('#carta-texto').value === '') {
-    return (document.querySelector('#carta-gerada').innerText =
+    return (document.querySelector('#carta-gerada').innerHTML =
       'por favor, digite o conteúdo da carta.');
   }
   if (
-    document.getElementById('carta-gerada').innerText ===
+    document.getElementById('carta-gerada').innerHTML ===
     'por favor, digite o conteúdo da carta.'
   ) {
     return (document.getElementById('carta-gerada').innerText = '');
   }
   gerarCartaMisteriosa();
   aplicarEstilo();
-  return (textoDaCarta = null);
+  aplicarEstiloNoClick();
 });
