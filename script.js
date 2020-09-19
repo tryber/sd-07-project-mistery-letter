a84229b026b35afbaa5d634fca4bb0a49aed333d
alvaro <alvarovascon2@gmail.com>
2020-09-19 16:45:24 -0300

    classes aleatorias

--- script.js
+++ script.js
@@ -2,14 +2,23 @@ const textInput = document.getElementById('carta-texto');
 const generatedLetter = document.getElementById('carta-gerada');
 const creationButton = document.getElementById('criar-carta');
 const wordCont = document.getElementById('carta-contador');
-const StyleGroupClasses = [`newspaper`, `magazine1`, `magazine2`];
+const styleGroupClasses = [`newspaper`, `magazine1`, `magazine2`];
 const sizeGroupClasses = [`medium`, `big`, `reallybig`];
 const rotationGroupClasses = [`rotateleft`, `rotateright`];
 const skewGroupClasses = [`skewleft`, `skewright`];
 
+function addRandomClasses (createdSpan) {
+  const randomStyle = Math.floor(Math.random() * styleGroupClasses.length);
+  const randomSize = Math.floor(Math.random() * sizeGroupClasses.length);
+  const randomRotation = Math.floor(Math.random() * rotationGroupClasses.length);
+  const randomSkew = Math.floor(Math.random() * skewGroupClasses.length);
+  createdSpan.classList = `${styleGroupClasses[randomStyle]} ${sizeGroupClasses[randomSize]} ${rotationGroupClasses[randomRotation]} ${skewGroupClasses[randomSkew]}`;
+}
+
 function createSpan(word) {
   const newSpan = document.createElement('span');
   newSpan.innerHTML = word;
+  addRandomClasses(newSpan);
   generatedLetter.appendChild(newSpan);
   generatedLetter.innerHTML += ' ';
 }
