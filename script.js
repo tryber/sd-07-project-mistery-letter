56badaedba12ad97fd8cd38acdd3de645c690286
Pablo Pessanha <pablopessanh@gmail.com>
2020-09-19 15:25:03 -0300

    All requests done; refactoring #5

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const containerCarta = document.querySelector('#carta-gerada');
+const contador = document.querySelector('#carta-contador');
 const novaCarta = document.querySelector('#criar-carta');
 const textoInserido = document.querySelector('#carta-texto');
 const tamanho = ['medium', 'big', 'reallybig'];
 const estilo = ['newspaper', 'magazine1', 'magazine2'];
 const rotacao = ['rotateleft', 'rotateright'];
 const inclinacao = ['skewleft', 'skewright'];
+let contagemFinal = 0;
 
 // Solução p/ espaçamento em branco encontrado no seguinte link: https://bit.ly/3iGxe4F
 function checarEspaco(texto) {
@@ -25,11 +27,11 @@ function escolhaRandomica() {
 
 novaCarta.addEventListener('click', () => {
   const caixaTexto = textoInserido.value;
-  let carta = document.createElement('span');
-  carta.id = 'criar-carta';
 
   if (checarEspaco(caixaTexto).length !== 0) {
     const arrayTexto = caixaTexto.split(' ', caixaTexto.length);
+    const contando = arrayTexto.length;
+    contagemFinal += contando;
 
     if (containerCarta.classList.contains('aviso')) {
       containerCarta.classList.remove('aviso');
@@ -38,7 +40,8 @@ novaCarta.addEventListener('click', () => {
 
     arrayTexto.forEach((textoSeparado) => {
       const classes = escolhaRandomica();
-      carta = document.createElement('span');
+      const carta = document.createElement('span');
+
       carta.id = 'criar-carta';
 
       classes.forEach((item) => {
@@ -48,8 +51,12 @@ novaCarta.addEventListener('click', () => {
       const texto = document.createTextNode(textoSeparado);
       carta.appendChild(texto);
       containerCarta.appendChild(carta);
+      contador.innerText = '';
+      contador.innerText = contagemFinal;
     });
   } else {
+    contagemFinal = 0;
+    contador.innerText = '0';
     containerCarta.innerText = '';
     const aviso = 'Por favor, digite o conteúdo da carta.';
     const texto = document.createTextNode(aviso);
