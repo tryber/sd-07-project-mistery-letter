0102491dd3a9744137500af8f39e5974b9618bd5
Carol Bezerra <carol.arbe@gmail.com>
2020-09-20 15:16:42 -0300

    Code Climate

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+function puttingStyle(spanHere) {
+  // Math.random() * (max - min) + min; igual ou maior a min e menor que max;
+  const styleClass = ['newspaper', 'magazine1', 'magazine2'];
+  const sizeClass = ['medium', 'big', 'reallybig'];
+  const rotateClass = ['rotateleft', 'rotateright'];
+  const skewClass = ['skewleft', 'skewright'];
+  const from0to3 = Math.floor(Math.random() * 3); // para gerar o índice aleatório;
+  const from0to2 = Math.floor(Math.random() * 2);
+  const style = styleClass[from0to3]; // para a linha 29 fica com menos de 100 (Code Climate);
+  const rotate = rotateClass[from0to2];
+  spanHere.className = `${style} ${sizeClass[from0to3]} ${rotate} ${skewClass[from0to2]}`;
+}
+
 document.querySelector('#criar-carta').addEventListener('click', function () {
   const frase = document.querySelector('#carta-texto').value;
   const word = frase.split(' ');
@@ -14,16 +27,3 @@ document.querySelector('#criar-carta').addEventListener('click', function () {
   }
   document.querySelector('#carta-contador').innerText = document.querySelectorAll('span').length;
 });
-
-function puttingStyle(spanHere) {
-  // Math.random() * (max - min) + min; igual ou maior a min e menor que max;
-  const styleClass = ['newspaper', 'magazine1', 'magazine2'];
-  const sizeClass = ['medium', 'big', 'reallybig'];
-  const rotateClass = ['rotateleft', 'rotateright'];
-  const skewClass = ['skewleft', 'skewright'];
-  const from0to3 = Math.floor(Math.random() * 3); // para gerar o índice aleatório;
-  const from0to2 = Math.floor(Math.random() * 2);
-  const style = styleClass[from0to3]; // para a linha 29 fica com menos de 100 (Code Climate);
-  const rotate = rotateClass[from0to2];
-  spanHere.className = `${style} ${sizeClass[from0to3]} ${rotate} ${skewClass[from0to2]}`;
-}
