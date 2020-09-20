cdaf5ce7762324c0dafbdefec0ed0b9091d5d309
Ana Karine <anakarine.ti@gmail.com>
2020-09-20 18:08:57 -0300

    requisitos 1, 2 e 3

---
+++ script.js
@@ -0,0 +1,19 @@
+var inputText = document.getElementById('carta-texto');
+var text = '';
+var words = [];
+var generateLetter = document.getElementById('carta-gerada');
+var btn = document.getElementById('criar-carta');
+
+btn.addEventListener('click', function () {
+  text = inputText.value;
+  span(text);
+  });
+
+function span(text) {
+  const arrayWords = text.split(' ');
+  for (let index = 0; index < arrayWords.length; index += 1) {
+    const createTagSpan = document.createElement('span');
+    createTagSpan.innerText = arrayWords[index]; 
+    generateLetter.appendChild(createTagSpan);
+  }
+}
