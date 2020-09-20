b393b8e333b40bb9b6c76a6af3d540399ebc7953
marcelosoliveira <msbobsk8@gmail.com>
2020-09-20 16:54:32 -0300

    Requisitos obrigatórios mistery-letter

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const criarCarta = document.querySelector('#criar-carta');
 const cartaTexto = document.querySelector("#carta-texto");
 
 criarCarta.addEventListener('click', () => {
-  if (cartaTexto.value === '') {
+    console.log(cartaTexto.value.trim());
+  if (cartaTexto.value === '' || !cartaTexto.value.trim()) {
     alert('Por favor, digite o conteúdo da carta.');
-  }    
-})
+   
+});
