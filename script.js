97cadaac51e7c5360e6de4a0717795f6e7f7fb45
kramersilva <kramerscs@gmail.com>
2020-09-20 14:47:00 -0300

    16 e 17

--- script.js
+++ script.js
@@ -7,9 +7,7 @@ console.log(paragraphChildren[1])
 
 function clearParagraph () {
   let paragraphChildren = letterParagraph.childNodes
-  console.log(paragraphChildren[0])
   for (let index = 0; index < paragraphChildren.length; index += 0) {
-    console.log(index)
     letterParagraph.removeChild(paragraphChildren[0])
   }
 }
@@ -22,12 +20,46 @@ function createLetter() {
       let newParagraphElement = document.createElement('span');
       newParagraphElement.innerText = words[item]
       letterParagraph.appendChild(newParagraphElement)
+      addRandomClass(newParagraphElement)
+      addClickListener(newParagraphElement)
     }
   } else {
     letterParagraph.innerText = 'Por favor, digite o conteúdo da carta.';
   }
 }
 
-
-
 createLetterButton.addEventListener('click', createLetter)
+
+function addRandomClass(newWord) {
+  const styleGroup = ["newspaper", "magazine1", "magazine2"]
+  const sizeGroup = ["medium", "big", "reallybig"]
+  const rotationGroup = ["rotateleft", "rotateright"]
+  const inclinationGroup = ["skewleft", "skewright"]
+
+  let randomStyle = Math.ceil(Math.random() * styleGroup.length)
+  let randomSize = Math.ceil(Math.random() * sizeGroup.length)
+  let randomRotation = Math.ceil(Math.random() * rotationGroup.length)
+  let randomInclination = Math.ceil(Math.random() * inclinationGroup.length)
+  
+  newWord.classList.add(styleGroup[randomStyle - 1])
+  newWord.classList.add(sizeGroup[randomSize - 1])
+  newWord.classList.add(rotationGroup[randomRotation - 1])
+  newWord.classList.add(inclinationGroup[randomInclination - 1])
+}
+
+
+function addClickListener(newWord) {
+  newWord.addEventListener('click', function () {
+    const styleGroup = ["newspaper", "magazine1", "magazine2"]
+    let randomStyle = Math.ceil(Math.random() * styleGroup.length)
+
+    if (newWord.classList.contains(styleGroup[0])) {
+      newWord.classList.remove(styleGroup[0])
+    } else if (newWord.classList.contains(styleGroup[1])) {
+      newWord.classList.remove(styleGroup[1])
+    } else if (newWord.classList.remove(styleGroup[2])) {
+      newWord.classList.remove(styleGroup[2])
+    }
+    newWord.classList.add(styleGroup[randomStyle - 1])
+  })
+}
