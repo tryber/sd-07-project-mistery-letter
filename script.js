c4b6654a1c530be285cf1b6d899cf66c08ab86b7
jpaula <paula.jfe@gmail.com>
2020-09-20 23:00:24 -0300

    Primeiros requisitos do projeto 2

--- script.js
+++ script.js
@@ -8,16 +8,21 @@ window.onload = function () {
 
     function getLetterDone() {
         let inputText = textOfLetter.value;
-        let eachWord = inputText.split(" ");
-        for (index = 0; index < eachWord.length; index += 1) {
-            let arrayWord = eachWord[index];
-            if (eachWord[index] !== "") {
-                let generateSpanTag = document.createElement("span");                                                                         
-                generateSpanTag.innerHTML = arrayWord;
-                createdLetter.appendChild(generateSpanTag);
-                console.log(generateSpanTag);
+        if (inputText !== "") {
+            let eachWord = inputText.split(" ");
+            for (index = 0; index < eachWord.length; index += 1) {
+                let arrayWord = eachWord[index];
+                if (eachWord[index] !== "") {
+                    let generateSpanTag = document.createElement("span");                                                                         
+                    generateSpanTag.innerHTML = arrayWord;
+                    createdLetter.appendChild(generateSpanTag);
+                    console.log(generateSpanTag);
+                }
             }
+        } else {
+            alert("Por favor, digite o conteúdo da carta.");
         }
+        
     }
     /* Cada palavra deve aparecer dentro de uma tag `span`.
 * As tags `span` devem ser adicionadas como filha do parágrafo que possui o id `carta-gerada`
