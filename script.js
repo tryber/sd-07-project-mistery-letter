fe6d5c5dd0364642373b3042819a33517e16c331
adrianoforcellini <adrianoforcellinipedretti@gmail.com>
2020-09-21 04:42:12 -0300

    primeira parte da lógica

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let textoAInserir = document.getElementById("carta-texto");
 let textoInserido = document.getElementById("carta-gerada");
 
 botaoCriar.addEventListener("click", function() {
-    textoInserido.innerHTML = textoAInserir.value;
-    textoInserido.classList.add("magazine1");
+    textoInserido.innerHTML = textoAInserir.value.split(" ");
+    for (i = 0; i < textoInserido.innerHTML.length; i++);
+    textoInserido[i].classList.add("magazine1");
 });
