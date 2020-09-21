75863844d31dfe7834c19c5555e9258249def98d
jpaula <paula.jfe@gmail.com>
2020-09-20 22:36:57 -0300

    Primeiros requisitos do projeto

---
+++ script.js
@@ -0,0 +1,26 @@
+window.onload = function () {
+
+    let textOfLetter = document.querySelector("#carta-texto"); /* input */
+    let createdLetter = document.querySelector("#carta-gerada"); /* p */
+    let createLetterButton = document.querySelector("#criar-carta"); /* button */
+
+    createLetterButton.addEventListener("click", getLetterDone);
+
+    function getLetterDone() {
+        let inputText = textOfLetter.value;
+        let eachWord = inputText.split(" ");
+        for (index = 0; index < eachWord.length; index += 1) {
+            let arrayWord = eachWord[index];
+            if (eachWord[index] !== "") {
+                let generateSpanTag = document.createElement("span");                                                                         
+                generateSpanTag.innerHTML = arrayWord;
+                createdLetter.appendChild(generateSpanTag);
+                console.log(generateSpanTag);
+            }
+        }
+    }
+    /* Cada palavra deve aparecer dentro de uma tag `span`.
+* As tags `span` devem ser adicionadas como filha do parágrafo que possui o id `carta-gerada`
+} */
+
+}
