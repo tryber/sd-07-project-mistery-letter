4a0b17e502e14ad3e8015ccb14be37956fed0a13
alexandrefaustino <alexandrefauldade@gmail.com>
2020-09-21 22:52:57 -0300

    commit: 0.0.9

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-function generationLetter(phrase) {
-  console.log(phrase);
+function createTagSpan(size) {
+  console.log(size);
+}
+
+function generatingArrayOfWords(phrase) {
+  const arrayWords = phrase.split(" ");
+  return arrayWords;
+}
+
+function generationLetter(boolean, phrase) {
+  if(boolean){
+    const arrayWords = generatingArrayOfWords(phrase);
+    createTagSpan(arrayWords.length);
+  }
 }
 
 function checksInputEmpty(inputText) {
   if (inputText.value === "") {
     document.getElementById('carta-gerada').innerText = 'Por favor, digite o conteúdo da carta';
+    return false;
   }
+  return true;
 }
 
 document.getElementById('criar-carta').addEventListener('click', function () {
   const inputText = document.querySelector('#carta-texto')
-  checksInputEmpty(inputText);
-  generationLetter(inputText.value);
-  return true;
+  const resultBoolean = checksInputEmpty(inputText);
+  generationLetter(resultBoolean, inputText.value);
 });
