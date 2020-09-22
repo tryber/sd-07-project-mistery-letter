72920c38c4adb312b1c1e137df3f19813a24781f
marcelosoliveira <msbobsk8@gmail.com>
2020-09-22 10:26:56 -0300

    code climate mistery letter

--- script.js
+++ script.js
@@ -9,38 +9,37 @@ const rot = ['rotateleft', 'rotateright'];
 const inc = ['skewleft', 'skewright'];
 
 function geraClasses() {      
-   const estilos = est[Math.round(Math.random() * (est.length - 1))];
-   const tamanhos = tam[Math.round(Math.random() * (tam.length - 1))];
-   const rotacao = rot[Math.round(Math.random() * (rot.length - 1))];
-   const inclina = inc[Math.round(Math.random() * (inc.length - 1))];
-   let classes = [estilos, tamanhos, rotacao, inclina];
-   let classesRandom = classes[Math.round(Math.random() * (classes.length - 1))];
-   return classesRandom;
+  const estilos = est[Math.round(Math.random() * (est.length - 1))];
+  const tamanhos = tam[Math.round(Math.random() * (tam.length - 1))];
+  const rotacao = rot[Math.round(Math.random() * (rot.length - 1))];
+  const inclina = inc[Math.round(Math.random() * (inc.length - 1))];
+  const classes = [estilos, tamanhos, rotacao, inclina];
+  const classesRandom = classes[Math.round(Math.random() * (classes.length - 1))];
+  return classesRandom;
 }
 
 criarCarta.addEventListener('click', () => {
-    if (cartaTexto.value === '' || !cartaTexto.value.trim()) {
-        cartaGerada.innerText = 'Por favor, digite o conteúdo da carta.';
-    } else {
-        let count = 0;
-        cartaGerada.innerText = '';
-        let textoCarta = cartaTexto.value.trim().split(' ');
+  if (cartaTexto.value === '' || !cartaTexto.value.trim()) {
+    cartaGerada.innerText = 'Por favor, digite o conteúdo da carta.';
+  } else {
+    let count = 0;
+    cartaGerada.innerText = '';
+    let textoCarta = cartaTexto.value.trim().split(' ');
     for (let index = 0; index < textoCarta.length; index += 1) {
-        let spans = document.createElement('span');
-        spans.style.display = 'inline-block';
-        spans.classList.add(geraClasses());
-        spans.classList.add(geraClasses());
-        spans.classList.add(geraClasses());
-        spans.classList.add(geraClasses());
-        if (textoCarta[index].trim()) {
-            spans.innerText = textoCarta[index];
-            cartaGerada.appendChild(spans);
-            count += 1;
-            contador.innerText = `${count}`;
-        }
+      let spans = document.createElement('span');
+      spans.style.display = 'inline-block';
+      spans.classList.add(geraClasses());
+      spans.classList.add(geraClasses());
+      spans.classList.add(geraClasses());
+      spans.classList.add(geraClasses());
+      if (textoCarta[index].trim()) {
+        spans.innerText = textoCarta[index];
+        cartaGerada.appendChild(spans);
+        count += 1;
+        contador.innerText = `${count}`;
+      }
     }
-    }
-    
+  }
 });
 
 cartaGerada.addEventListener('click', (event) => {
