42517893b919ef72dfa9a32eebb6a073226b3aa8
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2020-09-23 04:42:30 -0300

    Finalizando Requisitos Bonus - 23/09/2020

--- script.js
+++ script.js
@@ -27,8 +27,9 @@ function createLetter() {
         break;
       }
     }
-
     addClassList();
+    changeStyle();
+    count();
   });
 }
 
@@ -39,11 +40,11 @@ const arrayInclination = [`skewleft`, `skewright`];
 
 function addClassList() {
   const span = document.querySelectorAll("span");
-  
+
   for (let i = 0; i < span.length; i += 1) {
     const randomNumber1 = Math.round(Math.random() * 2);
     const randomNumber2 = Math.round(Math.random() * 1);
-    console.log(randomNumber1);
+    // console.log(randomNumber1);
 
     span[i].classList.add(arrayStyle[randomNumber1]);
     span[i].classList.add(arraySize[randomNumber1]);
@@ -51,6 +52,31 @@ function addClassList() {
     span[i].classList.add(arrayInclination[randomNumber2]);
   }
   // console.log(arrayStyle[randomNumber]);
+}
+
+function changeStyle() {
+  const span = document.querySelectorAll("span");
+
+  for (let i = 0; i < span.length; i += 1) {
+    span[i].addEventListener("click", function (event) {
+      const word = event.target;
+      const randomNumber1 = Math.round(Math.random() * 2);
+      const randomNumber2 = Math.round(Math.random() * 1);
+
+      word.classList = "";
+      word.classList.add(arrayStyle[randomNumber1]);
+      word.classList.add(arraySize[randomNumber1]);
+      word.classList.add(arrayRotation[randomNumber2]);
+      word.classList.add(arrayInclination[randomNumber2]);
+    });
+  }
+}
+
+function count() {
+  const countWord = document.querySelector("#carta-contador");
+  const span = document.querySelectorAll("span");
+
+  countWord.innerText = `Contador de Palavras: ${span.length}`;
 }
 
 window.onload = function () {
