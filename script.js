eb1ad4e777f7aa6dd2a001034188187826f1dd88
Ana Karine <anakarine.ti@gmail.com>
2020-09-23 00:45:46 -0300

    classes aleatorias

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 var inputText = document.getElementById('carta-texto');
 var text;
-var words = [];
+
 var generateLetter = document.getElementById('carta-gerada');
 var btn = document.getElementById('criar-carta');
+
+const style = ['newspaper', 'magazine1', 'magazine2'];
+const size = ['medium', 'big', 'reallybig'];
+const rotate = ['rotateleft', 'rotateright'];
+const inclination = ['skewleft', 'skewright'];
+
+
+function styleGenerator(text) {
+  const x = style[Math.round(Math.random()*2)];
+  const y = size[Math.round(Math.random()*2)];
+  const z = rotate[Math.round(Math.random()*1)];
+  const w = inclination[Math.round(Math.random()*1)];
+  text.className = `${x} ${y} ${z} ${w}`;
+}
+
 
 btn.addEventListener('click', function () {
   text = inputText.value;
@@ -19,5 +34,6 @@ function span(text) {
     const createTagSpan = document.createElement('span');
     createTagSpan.innerText = arrayWords[index]; 
     generateLetter.appendChild(createTagSpan);
-  }
+    styleGenerator(createTagSpan);
+  } 
 }
