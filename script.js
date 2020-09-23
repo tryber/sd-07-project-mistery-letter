73995d6a0d7379d204670ab197da7c32fbf62688
Ana Karine <anakarine.ti@gmail.com>
2020-09-23 10:37:33 -0300

    tentando atender o avaliador

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-function styleGenerator(txt) {
-  const style = ['newspaper', 'magazine1', 'magazine2'];
-  const size = ['medium', 'big', 'reallybig'];
-  const rotate = ['rotateleft', 'rotateright'];
-  const inclination = ['skewleft', 'skewright'];
+const style = ['newspaper', 'magazine1', 'magazine2'];
+const size = ['medium', 'big', 'reallybig'];
+const rotate = ['rotateleft', 'rotateright'];
+const inclination = ['skewleft', 'skewright'];
 
-  const x = style[Math.round(Math.random() * 2)];
-  const y = size[Math.round(Math.random() * 2)];
-  const z = rotate[Math.round(Math.random() * 1)];
-  const w = inclination[Math.round(Math.random() * 1)];
+function styleGenerator(txt) {
+  const num1 = Math.round(Math.random() * 2);
+  const num2 = Math.round(Math.random() * 1);
+
+  const x = style[num1];
+  const y = size[num1];
+  const z = rotate[num2];
+  const w = inclination[num2];
   txt.className = `${x} ${y} ${z} ${w}`;
 }
 
@@ -17,13 +20,18 @@ document.querySelector('#criar-carta').addEventListener('click', function () {
   if (text === '' || text === ' ') {
     document.getElementById('carta-gerada').innerText = 'Por favor, digite o conteúdo da carta.';
   } else {
-    const arrayWords = text.split(' ');
-    for (let index = 0; index < arrayWords.length; index += 1) {
-      const createTagSpan = document.createElement('span');
-      createTagSpan.innerText = arrayWords[index];
-      document.getElementById('carta-gerada').appendChild(createTagSpan);
-      styleGenerator(createTagSpan);
-    }
-    document.querySelector('#carta-contador').innerText = arrayWords.length;
+    createLetter (text);
   }
 });
+
+
+function createLetter (text) {
+  const arrayWords = text.split(' ');
+  for (let index = 0; index < arrayWords.length; index += 1) {
+    const createTagSpan = document.createElement('span');
+    createTagSpan.innerText = arrayWords[index];
+    document.getElementById('carta-gerada').appendChild(createTagSpan);
+    styleGenerator(createTagSpan);
+  }
+  document.querySelector('#carta-contador').innerText = arrayWords.length;
+}
