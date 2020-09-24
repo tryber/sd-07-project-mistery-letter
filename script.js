13e41516f63fd181a553ce1d7676dd79271b2a22
renatomak <renato.mark.silva@gmail.com>
2020-09-23 22:30:19 -0300

    estrutura basica do projeto

---
+++ script.js
@@ -0,0 +1,12 @@
+const cartaTexto = document.getElementById('carta-texto');
+const btnCriarCarta = document.getElementById('criar-carta');
+const ParCartaGerada = document.getElementById("carta-gerada");
+
+const geraCarta = function (){
+    
+}
+
+window.onload = function () {
+    btnCriarCarta.addEventListener('click', geraCarta());
+
+}
