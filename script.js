bff51c25b0915b801dccbcfaf8d708e00954b787
Joao Andrade Junior <fullservicemanaus@gmail.com>
2020-09-24 09:11:36 -0400

    criado teste pra que nao haja somente espaços em branco no campo input

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ botaoGerarCarta.addEventListener('click', function () {
   const textoDigitado = document.getElementById('carta-texto').value;
   const pSaidaTextoDigitado = document.getElementById('carta-gerada');
   const qtdLetras = document.getElementById('carta-contador');
-  if (textoDigitado === '') {
+  if (!textoDigitado.length || textoDigitado.trim() === '') {
     pSaidaTextoDigitado.innerText = 'Por favor, digite o conteúdo da carta.';
   } else {
     qtdLetras.innerText = textoDigitado.length;
