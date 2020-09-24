4ce5f4ba7b32126c46d99d7121fae9b253af9beb
Joao Andrade Junior <fullservicemanaus@gmail.com>
2020-09-24 09:30:18 -0400

    criada funçao para contar quantas palavras foram digitadas

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+function contaPalavras (frase) {
+    let cont = 0;
+    for (let i = 0; i < frase.length; i++) {
+        if (frase[i] === ' ') {
+            cont++;
+        }
+    }
+    return cont + 1;
+}
 const botaoGerarCarta = document.getElementById('criar-carta');
 botaoGerarCarta.addEventListener('click', function () {
   const textoDigitado = document.getElementById('carta-texto').value;
   const pSaidaTextoDigitado = document.getElementById('carta-gerada');
-  const qtdLetras = document.getElementById('carta-contador');
+  const qtdPalavras = document.getElementById('carta-contador');
   if (!textoDigitado.length || textoDigitado.trim() === '') {
     pSaidaTextoDigitado.innerText = 'Por favor, digite o conteúdo da carta.';
   } else {
-    qtdLetras.innerText = textoDigitado.length;
+    qtdPalavras.innerText = contaPalavras(textoDigitado);
     pSaidaTextoDigitado.innerText = textoDigitado;
     pSaidaTextoDigitado.classList.add('magazine2');
   }
