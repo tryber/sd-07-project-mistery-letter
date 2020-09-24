63ea9f41c3156cdb24acd78b35e76efc71f14928
Joao Andrade Junior <fullservicemanaus@gmail.com>
2020-09-24 08:24:48 -0400

    criada funçao para mostrar conteudo digitado e contador de caracteres

---
+++ script.js
@@ -0,0 +1,13 @@
+const botaoGerarCarta = document.getElementById('criar-carta');
+botaoGerarCarta.addEventListener('click', function () {
+  const textoDigitado = document.getElementById('carta-texto').value;
+  const pSaidaTextoDigitado = document.getElementById('carta-gerada');
+  const qtdLetras = document.getElementById('carta-contador');
+  if (textoDigitado === '') {
+    pSaidaTextoDigitado.innerText = 'Por favor, digite o conteúdo da carta.';
+  } else {
+    qtdLetras.innerText = textoDigitado.length;
+    pSaidaTextoDigitado.innerText = textoDigitado;
+    pSaidaTextoDigitado.classList.add('magazine2');
+  }
+});
