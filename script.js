a6d9cf00cae68d511c7a6e2d20f2e527505eb8b0
renatomak <renato.mark.silva@gmail.com>
2020-09-26 21:59:33 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -1,26 +1,40 @@
-const inputCartaTexto = document.getElementById('carta-texto');
 const btnCriarCarta = document.getElementById('criar-carta');
 
 
 const criaSpan = function (str) {
-    let newSapn = document.createElement('span');
-    newSapn.textContent = str;
-    
-    return newSapn;
+  let newSapn = document.createElement('span');
+  newSapn.textContent = str;
+
+  return newSapn;
 }
 
-const geraCarta = function (){
+const checkText = function () {
+    let inputText = document.getElementById('carta-texto').value;
+    inputText = inputText.trim();
+
+    if (inputText.length === 0) {
+      return false;
+    }
+    return true;
+  }
+
+const generateLetter = function () {
+  if (checkText()) {
+    const inputCartaTexto = document.getElementById('carta-texto');
     let text = inputCartaTexto.value.split(' ');
     console.log(text);
     const elemPai = document.getElementById("carta-gerada");
 
     text.forEach(word => {
-        const newSpan = criaSpan(word);
-        elemPai.appendChild(newSpan);
+      const newSpan = criaSpan(word);
+      elemPai.appendChild(newSpan);
     });
+  } else {
+    alert('Por favor, digite o conteúdo da carta.');
+  }
 }
 
 window.onload = function () {
-    btnCriarCarta.addEventListener('click', geraCarta);
+  btnCriarCarta.addEventListener('click', generateLetter);
 
 }
