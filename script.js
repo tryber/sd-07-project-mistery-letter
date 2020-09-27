f36bd7412be47387608850a92cf453ff2d371b8b
renatomak <renato.mark.silva@gmail.com>
2020-09-26 21:27:53 -0300

    Requisito 3

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-const cartaTexto = document.getElementById('carta-texto');
+const inputCartaTexto = document.getElementById('carta-texto');
 const btnCriarCarta = document.getElementById('criar-carta');
-const ParCartaGerada = document.getElementById("carta-gerada");
+
+
+const criaSpan = function (str) {
+    let newSapn = document.createElement('span');
+    newSapn.textContent = str;
+    
+    return newSapn;
+}
 
 const geraCarta = function (){
-    
+    let text = inputCartaTexto.value.split(' ');
+    console.log(text);
+    const elemPai = document.getElementById("carta-gerada");
+
+    text.forEach(word => {
+        const newSpan = criaSpan(word);
+        elemPai.appendChild(newSpan);
+    });
 }
 
 window.onload = function () {
-    btnCriarCarta.addEventListener('click', geraCarta());
+    btnCriarCarta.addEventListener('click', geraCarta);
 
 }
